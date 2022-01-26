4e305542eb532abae292e7d67e854d7d2cd53fcb
noomadglobal <90351514+noomadglobal@users.noreply.github.com>
2022-01-26 15:57:39 -0300

    Add files via upload

---
+++ components/aftersales/js/config.js
@@ -0,0 +1,114 @@
+var FORM_TYPE = {
+    CorporateSales: "01241000001IHZ2",
+    Sales: "01241000001IHYn",
+    AfterSales: "01241000001IHZ7"
+};
+
+var VEHICLE_CODES = [{
+    from: [
+        "8XFBLG",
+        "8XFAJG"
+    ],
+    to: "A1111"
+},{
+    from: [
+        "8V7B6G",
+        "8VMLCX",
+        "8VML8X",
+        "A3TFSI",
+        "8VAABG",
+        "8VFABG"
+    ],
+    to: "A3333"
+},{
+    from: [
+        "8W5BDG",
+        "8W2BAY",
+        "8W2ADG",
+        "8W2BDG",
+    ],
+    to: "A4444"
+},{
+    from: [
+        "4GJ02Y",
+        "4GC08G",
+        "4GC02Y"
+    ],
+    to: "A6666"
+},{
+    from: [
+        "4GF08G",
+        "4GF02Y"
+    ],
+    to: "A7777"
+},{
+    from: [
+        "4HL01A",
+        "4HL0GA",
+        "4HC0CA"
+    ],
+    to: "A8888"
+},{
+    from: [
+        "8UGA8Y",
+        "8UGCEY",
+        "8UGLKX",
+        "8UGLPX"
+    ],
+    to: "Q3333"
+},{
+    from: [
+        "8RB01A",
+        "8RB07A"
+    ],
+    to: "Q5555"
+},{
+    from: [
+        "4MB0L1",
+        "4MB0A1"
+    ],
+    to: "Q7777"
+},{
+    from: [
+        "8UGR7Y"
+    ],
+    to: "RSQ33"
+},{
+    from: [
+        "4GDRRA"
+    ],
+    to: "RS666"
+},{
+    from: [
+        "4GFRRA"
+    ],
+    to: "RS777"
+},{
+    from: [
+        "4S30BE"
+    ],
+    to: "R8888"
+},{
+    from: [
+        "8RBSCA"
+    ],
+    to: "Q5555"
+},{
+    from: [
+        "8VSS2L"
+    ],
+    to: "A3333"
+},{
+    from: [
+        "4HCS5A"
+    ],
+    to: "A8888"
+},{
+    from: [
+        "FV307X",
+        "FV907X",
+        "FV3S2L",
+        "FV9S2L"
+    ],
+    to: "1TTTT"
+}];
---
+++ components/audiforyou/js/form.js
@@ -0,0 +1,336 @@
+var Form;
+
+Form = (function(_window) {
+    'use strict';
+
+    var bind = function(fn, me) { 
+        return function() { 
+            return fn.apply(me, arguments);
+        };
+    };
+
+    function Form() {
+         this.onSubmit = bind(this.onSubmit, this);
+         this.onChangeVehicle = bind(this.onChangeVehicle, this);
+         this.campaignName = "";
+    };
+
+    Form.prototype.defaultParseQueryString = function () {
+        var vars = [], hash;
+        var hashes = window.location
+            .href
+            .slice(
+                window.location
+                    .href
+                    .indexOf('?') + 1
+            )
+            .split('&');
+
+        for(var i = 0; i < hashes.length; i++)
+        {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+
+        return vars;
+    };
+
+    Form.prototype.parseQuerystring = function() {
+        var args = this.defaultParseQueryString();
+        var webFormArguments = {
+            car: args.car ? args.car : "",
+            origin: args.origin ? args.origin : "",
+            subOrigin: args.subOrigin ? args.subOrigin : "",
+            campaign: args.campaign ? args.campaign : "",
+            leadSource: args.leadSource ? args.leadSource : "",
+            leadType: args.leadType ? args.leadType : "",
+            recordType: args.recordType ? args.recordType : ""
+        };
+
+        return webFormArguments;
+    };
+
+    Form.prototype.translateSpecificVehicle = function(carId){
+        var translatedId = null;
+        if(VEHICLE_CODES){
+            for (var i = 0; i < VEHICLE_CODES.length; i++) {
+                var vehicle = VEHICLE_CODES[i];
+
+                var index = vehicle.from.indexOf(carId);
+
+                if(index >= 0) {
+                    translatedId = vehicle.to;
+                    break;
+                }
+            }
+        }
+
+        return translatedId;
+    };
+
+    Form.prototype.changeCar = function() {
+        var vehicle = $("select[name=vehiclemodel]");
+
+        if(vehicle){
+            var itemFound = null;
+            var translatedId = this.translateSpecificVehicle(this.args.car);
+
+            var carId = translatedId
+                ? translatedId
+                : this.args.car;
+            
+            if(carId)
+                vehicle.children().each(function(index, item){
+                    if(item.value === carId){
+                        itemFound = item;
+                        return;
+                    }
+                });
+
+            if(itemFound){
+                vehicle.val(carId);
+                vehicle.attr({
+                    "disabled": "disabled"
+                });
+            } else {
+                this.args.car = "";
+            }
+        }
+    };
+
+    Form.prototype.onChangeVehicle = function (e) {
+        this.args.car = $(e.target).val();
+    };
+
+    Form.prototype.disableCompanyFields = function(){
+        this.company.attr({
+            'data-parsley-required': false
+        })
+        .parent()
+        .hide();
+
+        //dentro do app.js existe um evento que sobrepõe o .hide deste parent
+        //com o remove, não será necessário criar nenhuma gambiarra dentro do compilado 
+        //para controlar o estado de apresentação deste componente.
+        this.documentType
+            .parent()
+            .remove();
+    };
+
+    Form.prototype.findDocument = function() {
+        var documentType = $("input[name=documentType]:checked").data("id");
+        var document = "";
+
+        if(documentType)
+            document = documentType === 0
+                ? this.removeSpecialChars(this.cpf.val())
+                : this.removeSpecialChars(this.cnpj.val());
+        else 
+            document = this.removeSpecialChars(this.cpf.val());
+
+        return document;
+    };
+
+    Form.prototype.getCampaignName = function(campaign) {
+        return $.ajax({
+            type: 'get',
+            url: "https://audileads-new.herokuapp.com/campaign/getByIdSalesforce?id=" + campaign,
+            dataType: 'json'
+        });
+    };
+
+    Form.prototype.load = function () {
+        this.args = this.parseQuerystring();
+        this.flagGo = true;
+        this.form = $('form');
+        this.element = $('.formLead');
+        this.content = $('.formLeadContent');
+        this.success = $('.formLeadSuccess');
+        this.successMessageTitle = $('.formLeadSuccessTitle');
+        this.company = $('input[name=company]');
+        this.documentType = $('.formGroup.documentType div');
+        this.cpf = $("input[name=cpf]");
+        this.cnpj = $("input[name=cnpj]");
+
+        var sendFormButton = $('.send');
+        var vehicleSelect =  $('.vehiclemodel select');
+
+        vehicleSelect.on('change', this.onChangeVehicle);
+        sendFormButton.on('click', this.onSubmit);
+
+        if(this.args.recordType !== FORM_TYPE.CorporateSales)
+            this.disableCompanyFields();
+
+        if(this.args.campaign) {
+            var campaignPromise = this.getCampaignName(this.args.campaign);
+
+            $.when(campaignPromise).done(
+                bind(function(data){
+                    this.campaignName = data.campaign;
+                },this)
+            ).fail(function(error) {
+                console.log(error);
+            })
+        }
+    };
+
+    Form.prototype.removeSpecialChars = function(str){
+        return str.replace(/[^a-zA-Z 0-9]+/g,'');
+    };
+
+    Form.prototype.onSubmit = function(e) {
+        e.preventDefault();
+        //For checkbox validation.
+        $(".newsletter").removeClass("hasError");
+        if($(".newsletterCheckbox").find(".term-policy-error").hasClass("term-policy-error"))
+        {
+            $(".term-policy-error").remove();
+        }
+        if(!$(".newsletterCheckbox").hasClass("checked")){
+            $(".newsletter").addClass("hasError");
+            $(".newsletterCheckbox").append('<div class="custom-checkbox-error"><label class="term-policy-error"><span class="labelTitle" style="width: 100%; margin-top: 40px; background-color: transparent; display: block; text-align: right; font-family: AudiTypeV03-Normal; font-size: 13px;">*É necessário concordar com Declaração de Privacidade.</span></label></div>');
+        }else{
+            $(".term-policy-error").remove();
+        }
+
+        if (!this.form.parsley().validate()) {
+            this.element.find('.hasError').eq(0).find('select, input').focus();
+        }
+
+        if (this.form.parsley().isValid() && this.flagGo) {
+            this.flagGo = false;
+            this.element.find('.send').val('Enviando...');
+            var dealers = [];
+            var vehicles = [];
+
+            this.element.find('.seller select').each(function() {
+                if ($(this).val() !== '') {
+                return dealers.push($(this).val());
+                }
+            });
+
+            this.element.find('.vehiclemodel select').each(function() {
+                if ($(this).val() !== '') {
+                return vehicles.push($(this).val());
+                }
+            });
+
+            var name = this.element.find('.name input').val();
+            var firstName = '';
+            var lastName = '';
+
+            if(name && name.indexOf !== ' ' >= 0) {
+                var names = name.split(' ');
+                firstName = names[0];
+                names.splice(0, 1);
+                lastName = names.join(' ');
+            } else {
+                firstName = name;
+            }
+
+            var interestType = $('input[name=interest]:checked').data('id') == 1
+                ? "Information"
+                : "Buy";
+
+            var postData = {
+                "salutation": $('input[name=salutation]:checked').data('id'),
+                "first_name": firstName,
+                "00N4100000VtFGj": interestType === "Information"
+                    ? ""
+                    : this.findDocument(),
+                "00N4100000VtGXC": $('.birthday input').val(),
+                "phone": $('.phone input').val(),
+                "email": $('.email input').val(),
+                "optIn": $('.newsletterCheckbox').hasClass('checked') ? 1 : 0,
+                "00N4100000VtFBj": dealers[0],
+                "oid": "00D41000002ElAp",
+                "retURL": "http://www.audi.com.br/",
+                "recordType": this.args.recordType,
+                "00N4100000VtGX2": this.args.leadType,
+                "lead_source": this.args.leadSource,
+                "00N4100000VtGXR": interestType,
+                "00N4100000VtFDa": this.args.car,
+                "00N4100000VtFDz": $("select[name=vehiclemodel] option:selected").text(),
+                "00N4100000VtGde": this.args.subOrigin,
+                "00N4100000VtFE9": "",
+                "city": $('select[name=city] option:selected').text(),
+                "state": $('select[name=state] option:selected').text(),
+                "Campaign_ID": unescape(this.args.campaign),
+                "last_name": lastName,
+                "00N4100000bOzjS": sessionStorage.utm_campaign,
+                "00N4100000bOzjN": sessionStorage.utm_content,
+                "00N4100000bOzjD": sessionStorage.utm_medium,
+                "00N4100000bOziZ": sessionStorage.utm_source,
+                "00N4100000bOzjX": sessionStorage.utm_term
+            };
+
+            if(dealers.length > 1) {
+                postData["00N4100000VtFCm"] = dealers[1];
+            }
+
+            if($('.newsletterCheckbox').hasClass('checked')) {
+                postData["00N4100000VtGWs"] = "1";
+                postData["00N4100000XA0He"] = "1";
+                postData["00N4100000VtGWn"] = "1";
+            }
+
+            if(this.args.recordType === FORM_TYPE.CorporateSales)
+                postData["company"] = this.company.val();
+
+            $.ajax({
+                url: "https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8",
+                method: 'post',
+                data: postData,
+                error:  (function(_this) {
+                    return function(data, textStatus, jqXHR) {
+                        _this.successMessageTitle.html('<strong>Dados enviados com sucesso.</strong><br/>.');
+
+                        TweenMax.to(_this.success, 3, {
+                            autoAlpha: 1,
+                            ease: Quart.easeIn,
+                            onComplete: function() {
+                                _this.flagGo = true;
+
+                                $('body').scrollTop(0);
+
+                                _this.content.css({
+                                    'display': 'none'
+                                });
+
+                                _this.success.css({
+                                    'display': 'table-cell'
+                                });
+                            }
+                        });
+                    };
+                })(this),
+                success: (function(_this) {
+                    return function(jqXHR, textStatus, errorThrown) {
+                        _this.successMessageTitle.html('<strong>Dados enviados com sucesso.</strong><br/>.');
+
+                        TweenMax.to(_this.success, 3, {
+                            autoAlpha: 1,
+                            ease: Quart.easeIn,
+                            onComplete: function() {
+                                _this.flagGo = true;
+
+                                $('body').scrollTop(0);
+
+                                _this.content.css({
+                                    'display': 'none'
+                                });
+
+                                _this.success.css({
+                                    'display': 'table-cell'
+                                });
+                            }
+                        });
+                    };
+                })(this)
+            });
+        }
+    };
+
+    return Form;
+})();
